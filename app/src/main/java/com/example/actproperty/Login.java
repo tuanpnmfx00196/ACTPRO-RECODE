b9d4e553a001d66d59c2ee8c8f49f0c9d68f625d
tuanpnmfx00196 <phantuanqb85@gmail.com>
2021-04-25 02:17:12 +0700

    ...

--- app/src/main/java/com/example/actproperty/Login.java
+++ app/src/main/java/com/example/actproperty/Login.java
@@ -7,8 +7,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.actproperty.passport.GetPassport;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.actproperty.passport.Passport;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -16,7 +25,7 @@ import java.util.Collections;
 public class Login extends AppCompatActivity {
     Button btnLogin;
     EditText edtUser, edtPass;
-    ArrayList listPassport;
+    ArrayList<Passport> listPassport;
     ArrayList<Passport> listUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,15 +34,62 @@ public class Login extends AppCompatActivity {
         edtUser = (EditText) findViewById(R.id.edtUser);
         edtPass = (EditText) findViewById(R.id.edtPass);
         listUser = new ArrayList<>();
-        final GetPassport getPassport = new GetPassport();
+        listPassport = new ArrayList<>();
+        GetListPassport();
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Login.this, "Size "+getPassport.GetListPassport().size(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(Login.this, "Size "+listPassport.size(), Toast.LENGTH_SHORT).show();
             }
         });
-        /*============================= CHANGE PASSWORD ==============================*/
+            }
+        private void GetListPassport(){
+            String url="https://sqlandroid2812.000webhostapp.com/getpassport.php";
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            for(int i=0;i<response.length();i++){
+                                try {
+                                    JSONObject jsonObject = response.getJSONObject(i);
+                                    listPassport.add(new Passport(
+                                            jsonObject.getInt("ID"),
+                                            jsonObject.getString("USER"),
+                                            jsonObject.getString("PASSWORD"),
+                                            jsonObject.getString("FULLNAME"),
+                                            jsonObject.getString("MOBILE"),
+                                            jsonObject.getInt("ADMIN"),
+                                            jsonObject.getInt("PROPERTY"),
+                                            jsonObject.getInt("NOC"),
+                                            jsonObject.getInt("ACCOUNTANT"),
+                                            jsonObject.getInt("BDG"),
+                                            jsonObject.getInt("HCM_BCH"),
+                                            jsonObject.getInt("HCM_BTN"),
+                                            jsonObject.getInt("HCM_CCI"),
+                                            jsonObject.getInt("HCM_Q12"),
+                                            jsonObject.getInt("HCM_HMN"),
+                                            jsonObject.getInt("HCM_GVP"),
+                                            jsonObject.getInt("DNI"),
+                                            jsonObject .getInt("LAN"),
+                                            jsonObject.getInt("BTE"),
+                                            jsonObject.getInt("TVH"),
+                                            jsonObject.getInt("KGG")
+
+                                    ));
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+        }
 
     }
-
-}
